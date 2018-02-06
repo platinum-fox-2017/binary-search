bf9b035ad8362f75c0063b8746956e457117419f
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 14:44:21 +0700

    binary search

--- binary.js
+++ binary.js
@@ -4,26 +4,72 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  
+  let arrayNumb = arr
+
+  for(let i = 0;i < arrayNumb.length; i++){
+    let nilaiMinimum = arrayNumb[i]
+    let nilaiMinimumIndex = i
+    let count = 0
+    for(let j = i+1;j < arrayNumb.length;j++){
+      
+       if(arrayNumb[j] < nilaiMinimum){
+        nilaiMinimum = arrayNumb[j]
+        nilaiMinimumIndex = j
+        count++
+       } 
+    }
+
+    if(count > 0){
+      let tmp = arrayNumb[i]
+      arrayNumb[i] = arrayNumb[nilaiMinimumIndex]
+      arrayNumb[nilaiMinimumIndex] = tmp
+    }
+  }
+
+  return arrayNumb
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array, min, max) {
+  let cutMiddle = Math.floor((min+max)/2)
+  let maximum = max
+  let minimum = min
+
+
+  if(maximum < minimum && array[cutMiddle] !== search){
+    return false
+  }
+
+  if(array[cutMiddle] === search){
+    return true
+  } else{
+    
+    if(array[cutMiddle] < search){
+      minimum = cutMiddle + 1
+      return binary_search(search,array,minimum,maximum)
+    } 
+    
+    else if(array[cutMiddle] > search){
+      maximum = cutMiddle - 1
+      return binary_search(search,array,minimum,maximum)
+    } 
+  }
+  
+  
+  
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+// console.log(binary_search(8, arrayGenapSorted, 0, arrayGenapSorted.length - 1))
+// console.log(binary_search(10, arrayGenapSorted, 0, arrayGenapSorted.length - 1))
+// console.log(binary_search(33, arrayGenapSorted, 0, arrayGenapSorted.length - 1))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted, 0, arrayGanjilSorted.length - 1))
+console.log(binary_search(3, arrayGanjilSorted, 0, arrayGanjilSorted.length - 1))
+console.log(binary_search(2, arrayGanjilSorted, 0, arrayGanjilSorted.length - 1))
 
 module.exports = {
   binary_search
