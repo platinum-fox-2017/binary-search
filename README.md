aeb97621a517591b16ffe4e2192e0023ed7b2ac9
unknown <fatimah.azzhr@gmail.com>
2018-02-06 18:34:42 +0700

    binary release 0&1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # binary-search
+
+1. Set starting poin(start) mulai dari index ke-0
+2. Ending poin(end) merupakan index terakhir (array.length-1)
+3. Cari nilai tengahnya(mid) dengan menjumlah end dan start kemudian dibagi dengan 2
+4. Looping array selama nilai end lebih kecil dari nilai start atau sama dengan nol
+      Nilai mid = jumlah start dan end dibagi 2
+   5. Jika array index ke-mid sama dengan search
+        return nilai mid
+      Jika nilai search lebih besar dari nilai array index ke-mid
+        ubah start menjadi index setelah mid(mid+1) // ambil nilai array mulai dari mid+1 sampai akhir
+      Jika nilai search lebih kecil dari nilai array index ke-mid
+        ubah end menjadi index sebelum mid (mid-1) // ambil nilai array mulai dari awal (0) sampai mid-1
+   End loop
+6. Jika search tidak ditemukan return -1
--- binary.js
+++ binary.js
@@ -4,27 +4,63 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  var result = [];
+  var currentMin = arr[0];
+  var indexMin = 0;
+
+  if(arr.length === 1) {
+    return arr;
+  }
+
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] < currentMin) {
+      currentMin = arr[i];
+      indexMin = i;
+    }
+  }
+  
+  result.push(currentMin);
+  arr.splice(indexMin, 1);
+  var finalResult = result.concat(ownSort(arr));
+  return finalResult;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binarySearch(search, array) {
+  var start = 0;
+  var end = array.length - 1;
+  var mid;
+
+  while(start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (array[mid] === search) {
+      return mid;
+    } else if (search > array[mid]) {
+      start = mid+1;
+    } else {
+      end = mid-1;
+    }
+  }
+
+  return -1;
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
+
+console.log(arrayGenapSorted); // [ 8, 10, 10, 18, 22, 22, 32, 40, 90 ]
+console.log(arrayGanjilSorted); // [ 3, 21, 31, 53, 53, 55, 77, 85, 89 ]
+console.log(ownSort([33, 2, 52, 106, 73])) // [ 2, 33, 52, 73, 106 ]
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(73, [ 2, 33, 52, 73, 106 ]) === 3) // 3, true
+console.log(binarySearch(8, arrayGenapSorted) === 0) // 0, true
+console.log(binarySearch(10, arrayGenapSorted) === 1) // 1, true
+console.log(binarySearch(33, arrayGenapSorted) === -1) // -1, true
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted) === 3) // 3, false --> output: 4
+console.log(binarySearch(3, arrayGanjilSorted) === 0) // 0, true
+console.log(binarySearch(2, arrayGanjilSorted) === -1) // -1, true
 
 module.exports = {
-  binary_search
+  binarySearch
 }
