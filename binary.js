d0ddc89e99b51e526bc9ece3e4067e58df8a2fb9
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-06 21:50:28 +0700

    binary done

--- binary.js
+++ binary.js
@@ -4,25 +4,46 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for(var i=1; i<arr.length; i++){
+    for(var j=0; j<=i-1; j++){
+      var belakang = arr[i];
+      var depan = arr[j];
+      if(arr[i]<arr[j]){
+        arr[j]=belakang;
+        arr[i]=depan;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var bot = 0;
+  var top = array.length-1;
+  while(bot<=top){
+    var mid = Math.floor((bot+top)/2);
+    if(search===array[mid]){
+      return mid;
+    } else {
+      if(search>array[mid]){
+        bot = mid+1;
+      } else {
+        top = mid-1;
+      }
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-
+//
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(18, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
 console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(85, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = {
