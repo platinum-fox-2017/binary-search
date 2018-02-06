c85b290dd5043cf42c29d07a941a516b94e8bed1
agungcaproex <agung.caproex@gmail.com>
2018-02-06 18:01:24 +0700

    binary-search

--- binary.js
+++ binary.js
@@ -4,17 +4,41 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  var temp = []
+  var minIndex = ""
+
+  for(var i=0; i<arr.length; i++){
+    minIndex = i
+    for(var j=i+1; j<arr.length; j++){
+      if(arr[j] < arr[minIndex]){
+        minIndex = j
+      }
+    }
+    temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+  }
+
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
-}
+console.log(ownSort(testArrayGenap))
+console.log(ownSort(testArrayGanjil))
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+function binary_search (search, array) {
+  // Your searching code
+  for(var i=0; i<array.length; i++){
+    if(array[i] === search){
+      return i;
+    }
+  }
+  return -1;
+}
+
+
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
