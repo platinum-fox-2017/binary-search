2a95e851a4149751d696c4203584f8f43817b1fd
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-06 14:56:55 +0700

    release 0

--- binary.js
+++ binary.js
@@ -4,13 +4,30 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  for (var i = 1; i < arr.length; i++) {
+  	for (var j = 0; j < i; j++) {
+  		if (arr[i] < arr[j]) {
+  			var temp = arr[i];
+  			arr[i] = arr[j];
+  			arr[j] = temp;
+  		}
+  	}
+  }
+  console.log(arr);
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  if (array.length <= 1) {
+  	if (search == array[0]) return 0;
+  	else return -1;
+  }
+
+  var mid = Math.floor(array.length / 2);
+
+  if (search == array[mid]) return mid; else
+  if (search < array[mid]) return binary_search(search, array.splice(0, mid)); else
+  if (search > array[mid]) return binary_search(search, array.splice(mid, array.length - mid));
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
