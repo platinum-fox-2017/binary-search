9ba77268abc411afad0e3a2e7486c6337878a08b
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-06 14:15:39 +0700

    Add binary.js

--- binary.js
+++ binary.js
@@ -4,13 +4,36 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  for (var i = 1; i < arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        var temp = arr[j];
+        arr[j]   = arr[i];
+        arr[i]   = temp;
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var middleIndex = Math.round(array.length / 2);
+  var top = 0;
+  var bottom = array.length;
+
+  while (top <= bottom) {
+    if (search > array[middleIndex]) {
+      top = middleIndex + 1;
+      middleIndex = Math.round((top+bottom) / 2);
+    } else if (search < array[middleIndex]) {
+      bottom = middleIndex - 1;
+      middleIndex = Math.round((top+bottom) / 2);
+    } else if (search == array[middleIndex]) {
+      return middleIndex;    
+    }
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
