a73d6beec6a59e3ea22bae21c8270eb2e1a73e85
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-06 15:01:00 +0700

    still issues on finding the first one

--- binary.js
+++ binary.js
@@ -5,25 +5,88 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  var minNum = arr[0];
+
+  for(var i = 0; i < arr.length; i++){
+    let temp;
+    let indexMin = findMin(arr.slice(i))+i;
+    temp = arr[i];
+    arr[i]=arr[indexMin]
+    arr[indexMin] = temp;
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function findMin(arr){
+  let min = arr[0];
+  let indexMin = 0;
+  for (var i = 0; i < arr.length; i++){
+    if(arr[i]<min){
+      min = arr[i];
+      indexMin = i;
+    }
+  }
+  return indexMin;
 }
+// Solution 1 - recursive
+function binary_search (search, array) {
+  // Your searching code
+  if(array.length>=1){
+    let midValue = Math.floor((array.length-1)/2);
+    debugger
+    if(search==array[midValue]){
+      return midValue;
+    }
+    else if(search<array[midValue]){
+      return binary_search(search, array.slice(0,midValue));
+    }
+    else
+      return binary_search(search, array.slice(midValue+1));
+  }
+  else {
+    return -1;
+  }
+}
+
+// Solution 2
+function binary_search (search, array) {
+  // Your searching code
+  let bottom = 0;
+  let top = array.length-1;
+  while(bottom<=top){
+    let mid = Math.floor((top+bottom)/2);
+    if(search==array[mid])
+      return mid;
+    else if(search>array[mid]){
+      bottom = mid+1;
+    }
+    else{
+      top = mid-1;
+    }
+  }
+  return -1;
+}
+
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
+
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
+console.log("jawaban :"+arrayGenapSorted.indexOf(8));
 console.log(binary_search(10, arrayGenapSorted))
+console.log("jawaban :"+arrayGenapSorted.indexOf(10));
 console.log(binary_search(33, arrayGenapSorted))
+console.log("jawaban :"+arrayGenapSorted.indexOf(33));
+
 
 console.log(binary_search(53, arrayGanjilSorted))
+console.log("jawaban :"+arrayGanjilSorted.indexOf(53));
 console.log(binary_search(3, arrayGanjilSorted))
+console.log("jawaban :"+arrayGanjilSorted.indexOf(3));
 console.log(binary_search(2, arrayGanjilSorted))
+console.log("jawaban :"+arrayGanjilSorted.indexOf(2));
 
 module.exports = {
   binary_search
