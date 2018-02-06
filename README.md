ca51a6122d9016f363e77a6fb7e964fe890ac779
Herby Herado <herby,herado@gmail.com>
2018-02-06 17:12:05 +0700

    Release 0 & Release 1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # binary-search
+
+## Release 1: Write the algorithm
+
+1. Prior to running binary search function, ensure array that will be processed has been sorted (ascending/descending)
+2. Create binary search function that takes a 'search' parameter, input array, and a default parameter 'midpoint' that can be stacked and returned upon being called
+
+2.a Initialize a variable that takes the middle/center index of the array
+
+2.b set an (IF) conditional statement such that if array at center index is equals to 'search' parameter:
+    2.b.1 PRINT the sum of index at center of array plus the 'midpoint'
+2.c set an (ELSE IF) conditional statement when the search parameter value is less than value of array at center index AND array length is greater than 1:
+    2.c.1 return (Recursively) the function with the search parameter and new array that comprises values from index at start to middle.
+2.d set an (ELSE IF) conditional statement when the search parameter value is  greater than value of array at center index AND array length is greater than 1:
+    2.d.1 accumulate 'midpoint' variable (step 2.a) with length of array at starting index to middle
+    2.d.2 return (Recursively) the function with the search parameter, new array with values from center to end, and the midpoint value.
+2.e other than the above conditional statement, would return -1
+
--- binary.js
+++ binary.js
@@ -1,29 +1,54 @@
 'use strict'
-
+// Release 0
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  var temp;
+  var min;
+
+  for (var i = 0; i < arr.length-1; i++){
+    min = i;
+    for (var j = i+1; j < arr.length; j++){
+      if (arr[min] > arr[j]){
+        min = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array, midpoint = 0) {
   // Your searching code
-  return 0;
+  var midIdx = Math.floor(array.length/2);
+  if (array[midIdx] === search){
+    return midIdx + midpoint;
+  } else if (search < array[midIdx] && array.length > 1){
+    return binary_search(search, array.slice(0, midIdx))
+  } else if (search > array[midIdx] && array.length > 1){
+    midpoint += array.slice(0,midIdx).length;
+    return binary_search(search, array.slice(midIdx), midpoint)
+  } else {
+    return -1;
+  }
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted)) // 0
+console.log(binary_search(10, arrayGenapSorted)) // 2
+console.log(binary_search(33, arrayGenapSorted)) // -1
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted)) // 4
+console.log(binary_search(3, arrayGanjilSorted)) // 0
+console.log(binary_search(2, arrayGanjilSorted)) // -1
+console.log(binary_search(89, arrayGanjilSorted)) // 8
 
 module.exports = {
   binary_search
