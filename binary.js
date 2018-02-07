4f1e0f2e82569b9bed95606139cbc996cae1fc21
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-07 14:41:47 +0700

    refactor

--- binary.js
+++ binary.js
@@ -19,36 +19,34 @@ console.log(ownSort(testArrayGenap));
 console.log(ownSort(testArrayGanjil));
 
 function binary_search (search, array) {
-  // Your searching code
   let first = 0
   let last = array.length-1
   let mid = Math.floor((first+last) / 2)
 
-  for (var i = 0; i <= array.length-1; i++) {
-    if (search === array[mid]) {
-      return mid
-    }
-    else if (search > array[mid]) {
-      first = mid +1
-      mid = Math.floor((first+last) / 2)
-    }
-    else if (search < array[mid]) {
-      last = mid
-      mid = Math.floor((first+ last) / 2)
-    }
-
+  if (search === array[mid]) {
+    return mid
   }
-  return -1;
+  else if (search > array[mid]) {
+    first = mid +1
+    return binary_search(search, array.slice(first, last))
+  }
+  else if (search < array[mid]) {
+    last = mid
+    return binary_search(search, array.slice(first, last))
+  }
+  else {
+    return -1
+  }
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(8,  arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
+//
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
