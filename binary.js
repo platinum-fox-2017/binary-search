bd9d96192ecd1322f115393df99313c266cd792d
agnynureza <rezaagny@gmail.com>
2018-02-07 20:19:54 +0700

    benerin bug

--- binary.js
+++ binary.js
@@ -15,21 +15,20 @@ function ownSort(arr) {
   return arr
 }
 
-function binary_search (search, array) {
-  let first = 0
-  let last = array.length-1
+function binary_search (search,array,first,last) {
   let mid = Math.floor((first+last)/2)
 
-    if(array[mid] === search){
+    if(first > last && search !== array[mid] ){
+      return 'notfound '
+    }else if(array[mid] === search){
       return mid
     }else if (search < array[mid]){
-      return binary_search(search, array.slice(first,mid))
+      last = mid - 1
+      return binary_search(search, array,first,last)
     }else if(search > array[mid]){
-      return binary_search(search, array.slice(mid+1,last))
-    }else{
-      return -1
+      first = mid + 1
+      return binary_search(search, array,first,last)
     }
-
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -37,14 +36,14 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
 console.log(arrayGenapSorted)
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(100, arrayGenapSorted,0,arrayGenapSorted.length -1))
+console.log(binary_search(10, arrayGenapSorted,0,arrayGenapSorted.length -1))
+console.log(binary_search(40, arrayGenapSorted,0,arrayGenapSorted.length -1))
 
 console.log(arrayGanjilSorted)
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
+console.log(binary_search(3, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
+console.log(binary_search(2, arrayGanjilSorted,0,arrayGanjilSorted.length-1))
 
 module.exports = {
   // binarysearch
