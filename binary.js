2c4d4796abd63ddf6d57627e5ef553e59e7fe1f6
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-09 15:59:40 +0700

    remake sort

--- binary.js
+++ binary.js
@@ -5,41 +5,22 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  for (var i = 1; i < arr.length; i++) {
-    for (var j = i - 1; j >= 0; j--) {
-      if (arr[j + 1] < arr[j]) {
-        let firstNum = arr[j + 1]
-        let secondNum = arr[j]
-        arr[j + 1] = secondNum
-        arr[j] = firstNum
-      } else {
-        break;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        let swap = arr[i]
+        arr[i] = arr[j]
+        arr[j] = swap
       }
     }
   }
   return arr
 }
 
-function binary_search(search, array, obj) {
+function binary_search(search, array) {
   // Your searching code
-  obj = obj || {
-    min: 0,
-    max: array.length - 1
-  }
-  if (obj.min > obj.max) {
-    return -1
-  } else {
-    let nilaiTengah = Math.floor(obj.min + obj.max / 2);
-    if (array[nilaiTengah] === search) {
-      return nilaiTengah;
-    } else if (search < array[nilaiTengah]) {
-      obj.max = nilaiTengah - 1
-      return binary_search(search, array, obj)
-    } else {
-      obj.min = nilaiTengah + 1
-      return binary_search(search, array, obj)
-    }
-  }
+  
+  return 0;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -47,13 +28,13 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+// console.log(binary_search(10, arrayGenapSorted))
+// console.log(binary_search(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+// console.log(binary_search(53, arrayGanjilSorted))
+// console.log(binary_search(3, arrayGanjilSorted))
+// console.log(binary_search(2, arrayGanjilSorted))
 
-module.exports = {
-  binary_search
-}
+// module.exports = {
+//   binary_search
+// }
