fca94cf279c2a781828678d3e8a38bd7652ff653
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-09 16:32:48 +0700

    done fixing all error, reupload

--- binary.js
+++ binary.js
@@ -19,21 +19,40 @@ function ownSort(arr) {
 
 function binary_search(search, array) {
   // Your searching code
-  
-  return 0;
+  let low = 0,
+    high = array.length - 1,
+    mid;
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    if (array[mid] == search) return mid;
+    else if (array[mid] < search) low = mid + 1;
+    else high = mid - 1;
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
+let test_array = [13, 19, 24, 29, 32, 37, 43]
+console.log('ini test array', test_array)
+console.log(binary_search(35, test_array))
+console.log('===========')
+let test_array2 = [100, 120, 130, 135, 150, 170]
+console.log('ini test array 2', test_array2)
+console.log(binary_search(135, test_array2))
+console.log('===========')
+console.log('ini array genap sudah di sort', arrayGenapSorted)
+console.log('ini array ganjil sudah di sort', arrayGanjilSorted)
+console.log('===========')
 console.log(binary_search(8, arrayGenapSorted))
-// console.log(binary_search(10, arrayGenapSorted))
-// console.log(binary_search(33, arrayGenapSorted))
-
-// console.log(binary_search(53, arrayGanjilSorted))
-// console.log(binary_search(3, arrayGanjilSorted))
-// console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(10, arrayGenapSorted))
+console.log(binary_search(40, arrayGenapSorted))
+console.log('===========')
+console.log(binary_search(53, arrayGanjilSorted))
+console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(2, arrayGanjilSorted))
 
 // module.exports = {
 //   binary_search
